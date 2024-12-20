7b152ea5c224caad97a03afa060de58f9f741e25
Aquiles Trindade <trindadedev13@gmail.com>
2024-12-19 21:38:26 -0300

    feat: dragview

---
+++ app/src/main/java/dev/trindadedev/anims/ui/components/drag/DragState.kt
@@ -0,0 +1,7 @@
+package dev.trindadedev.anims.ui.components.drag
+
+data class DragState(
+  var dY: Float = 0f,
+  var dX: Float = 0f,
+  var duration: Int = 0
+)
---
+++ app/src/main/java/dev/trindadedev/anims/ui/components/drag/DragView.kt
@@ -0,0 +1,40 @@
+package dev.trindadedev.anims.ui.components.drag
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.MotionEvent
+import android.view.View
+
+class DragView @JvmOverloads 
+constructor(
+  context: Context,
+  attrs: AttributeSet? = null, 
+  defStyleAttr: Int = -1
+) : View(context, attrs, defStyleAttr) {
+  
+  var state = DragState()
+  
+  init {
+    setTouchListener { view, event ->
+      when (event.action) {
+        MotionEvent.ACTION_DOWN -> onDown(view, event)
+        MotionEvent.ACTION_MOVE -> onMove(view, event)
+        else -> false
+      }
+      return true
+    }
+  }
+  
+  private fun onDown(view: View, event: MotionEvent) {
+    state.dX = view.getX() - event.getRawX()
+    state.dY = view.getY() - event.getRawY()
+  }
+  
+  private fun onMove(view: View, event: MotionEvent) {
+    view.animate()
+      .x(event.getRawX() + state.dX)
+      .y(event.getRawY() + state.dY)
+      .setDuration(state.duration)
+      .start()
+  }
+}
