b0517a4224d3b9cfe64a5c2a3fc1348c31230f83
Aquiles Trindade <trindadedev13@gmail.com>
2024-12-20 03:28:30 -0300

    fix: crash

--- app/src/main/java/dev/trindadedev/learn/ui/scale/ScaleState.kt
+++ app/src/main/java/dev/trindadedev/learn/ui/scale/ScaleState.kt
@@ -5,6 +5,6 @@ import android.view.View
 data class ScaleState(
   val view: View,
   var scaleFactor: Float = 1.0f,
-  var scaleFactorMax: Float = 0.5f,
-  var scaleFactorMin: Float = 3.0f
+  var scaleFactorMin: Float = 0.5f,
+  var scaleFactorMax: Float = 3.0f
 )
