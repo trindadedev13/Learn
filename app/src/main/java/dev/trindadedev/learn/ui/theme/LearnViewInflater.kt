fee96a06c57f28b4487c4caf07b1118d45fd36a5
Aquiles Trindade <trindadedev13@gmail.com>
2024-12-20 01:13:47 -0300

    fix: null not accepted

--- app/src/main/java/dev/trindadedev/learn/ui/theme/LearnViewInflater.kt
+++ app/src/main/java/dev/trindadedev/learn/ui/theme/LearnViewInflater.kt
@@ -3,6 +3,10 @@ package dev.trindadedev.learn.ui.theme
 import android.content.Context
 import android.util.AttributeSet
 import android.view.View
+import android.widget.Switch
+
+import dev.trindadedev.learn.ui.components.drag.DragView
+
 import com.google.android.material.materialswitch.MaterialSwitch
 import com.google.android.material.theme.MaterialComponentsViewInflater
 
@@ -16,19 +20,17 @@ class LearnViewInflater: MaterialComponentsViewInflater() {
     attrs: AttributeSet
   ): View {
     return when (name) {
-      "Switch" -> MaterialSwitch(context, attrs)
-      else -> createComponent(context, name, attrs)
+      "Switch" -> createSwitch(context, attrs)
+      "DragView" -> createDragView(context, attrs)
+      else -> View(context, attrs)
     }
   }
   
-  /**
-   * Override this method in your class to continue inflater (if you want)
-   */
-  fun createComponent(
-    context: Context, 
-    name: String, 
-    attrs: AttributeSet
-  ): View {
-    return null
+  protected fun createDragView(context: Context, attrs: AttributeSet): DragView {
+    return DragView(context, attrs)
+  }
+  
+  protected fun createSwitch(context: Context, attrs: AttributeSet): Switch {
+    return MaterialSwitch(context, attrs)
   }
 }
