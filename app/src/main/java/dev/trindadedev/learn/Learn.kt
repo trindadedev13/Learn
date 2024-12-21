5a69958a5cb68036f9cb2add670e407f4d95387a
Aquiles Trindade <trindadedev13@gmail.com>
2024-12-21 02:58:54 -0300

    fix: rename app class

--- app/src/main/java/dev/trindadedev/learn/Learn.kt
+++ app/src/main/java/dev/trindadedev/learn/Learn.kt
@@ -3,7 +3,7 @@ package dev.trindadedev.learn
 import android.app.Application
 import com.google.android.material.color.DynamicColors
 
-class AnimsApp : Application() {
+class Learn : Application() {
   override fun onCreate() {
     DynamicColors.applyToActivitiesIfAvailable(this)
     super.onCreate()
